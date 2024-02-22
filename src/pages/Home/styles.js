6dfc272a6522dcb6e7f649cec52a9dbadf2c42c9
rodrigoaugusto99 <rodrigoaugusto51@outlook.com>
2024-02-22 19:54:01 -0300

    menu de filtros

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -20,11 +20,30 @@ export const Container = styled.div`
 `
 export const Brand = styled.div`
   grid-area: brand;
-  background-color: red;
+ 
+ display: flex;
+ justify-content: center;
+ align-items: center;
+
+ border-bottom-width: 1px;
+ border-bottom-style: solid;
+ border-bottom-color: ${({ theme }) => theme.COLORS.BACKGROUND_700};
+ background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+
+ > h1 {
+    font-size: 24px;
+    color: ${({ theme }) => theme.COLORS.ORANGE};
+ }
+
 `
 export const Menu = styled.ul`
   grid-area: menu;
-  background-color: green;
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+  padding-top: 64px;
+  text-align: center;
+  > li {
+    margin-bottom: 24px;
+  }
 `
 export const Search = styled.div`
   grid-area: search;
