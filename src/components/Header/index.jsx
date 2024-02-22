219745a115fded331d7e9396611fd50c9bbcc220
rodrigoaugusto99 <rodrigoaugusto51@outlook.com>
2024-02-22 00:05:09 -0300

    imagem de usuario

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,19 @@
-import { Container } from './styles'
+import { Container, Profile } from './styles'
 
 export function Header() {
   return (
     <Container>
+      <Profile>
+        <img
+          src="https://github.com/rodrigoaugusto99.png"
+          alt="Foto do usuário"
+        />
 
+        <div>
+          <span>Bem-vindo</span>
+          <strong>Rodrigo Gonçalves</strong>
+        </div>
+      </Profile>
     </Container>
   )
 }
