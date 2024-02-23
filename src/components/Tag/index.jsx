4d2bc100cec874b758144ee1ffefdb1a866f1c30
rodrigoaugusto99 <rodrigoaugusto51@outlook.com>
2024-02-23 14:17:55 -0300

    componente de notas

--- src/components/Tag/index.jsx
+++ src/components/Tag/index.jsx
@@ -1,8 +1,10 @@
 import { Container } from './styles'
 
-export function Tag({ title }) {
+export function Tag({ title , ...rest}) {
+  //esse ...rest joga tudo aqui tbm, ate essa key la feita em Note
+ // data.tags.map(tag => <Tag key={tag.id} title={tag.name} />)
   return (
-    <Container>
+    <Container {...rest}>
       {title}
     </Container>
   )
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,6 +2,8 @@ import { FiPlus } from 'react-icons/fi'
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
 
 import { Header } from '../../components/Header'
+import { Section } from '../../components/Section'
+import { Note } from '../../components/Note'
 import { ButtonText } from '../../components/ButtonText'
 import { Input } from '../../components/Input'
 
@@ -25,7 +27,16 @@ export function Home() {
       </Search>
 
       <Content>
-
+      <Section title="Minhas notas">
+          <Note data={{
+            title: 'React',
+            tags: [
+              { id: '1', name: 'react' },
+              { id: '2', name: 'rocketseat' }
+            ]
+          }}
+          />
+        </Section>
       </Content>
 
       <NewNote>
