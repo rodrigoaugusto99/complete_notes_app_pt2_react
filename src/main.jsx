19c673840a6408d76521f164945e47709560fa48
rodrigoaugusto99 <rodrigoaugusto51@outlook.com>
2024-02-23 18:35:45 -0300

    New, NoteItem, TextArea, Profile

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {Profile} from './pages/Profile'
+import {New} from './pages/New'
 
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-        <Profile />
+        <New  />
     </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/pages/New/index.jsx
@@ -0,0 +1,39 @@
+import { Header } from '../../components/Header'
+import { Input } from '../../components/Input'
+import { Container, Form } from './styles'
+import { Textarea } from '../../components/Textarea'
+import { NoteItem } from '../../components/NoteItem'
+import { Section } from '../../components/Section'
+import { Button } from '../../components/Button'
+
+export function New() {
+  return (
+    <Container>
+      <Header />
+      <main>
+        <Form>
+          <header>
+            <h1>Criar nota</h1>
+            <a href="/">voltar</a>
+          </header>
+
+          <Input placeholder="Título" />
+          <Textarea placeholder="Observações" />
+          <Section title="links úteis">
+            <NoteItem value="https://rocketseat.com.br" />
+            <NoteItem isNew placeholder="Novo link" />
+          </Section>
+
+          <Section title="Marcadores">
+            <div className="tags">
+              <NoteItem value="react" />
+              <NoteItem isNew placeholder="Nova tag" />
+            </div>
+          </Section>
+
+          <Button title="Salvar" />
+        </Form>
+      </main>
+    </Container>
+  )
+}
