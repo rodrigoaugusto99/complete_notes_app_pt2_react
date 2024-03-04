787828eb1db6809d3d05552dddfe0ef78c6dec7c
rodrigoaugusto99 <rodrigoaugusto51@outlook.com>
2024-03-04 20:02:12 -0300

    carregando dados do usuario e atualizando o profile

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -16,7 +16,8 @@ function AuthProvider({ children }){
             localStorage.setItem("@rocketnotes:user", JSON.stringify(user))
             localStorage.setItem("@rocketnotes:token", token)
 
-            api.defaults.headers.authorization = `Bearer ${token}`
+            //api.defaults.headers.authorization = `Bearer ${token}`
+            api.defaults.headers.common['Authorization'] = `Bearer ${token}`
             setData( {user, token})
 
         } catch (error) {
@@ -40,7 +41,7 @@ function AuthProvider({ children }){
         const user = localStorage.getItem("@rocketnotes:user")
 
         if(token && user){
-            api.defaults.headers.authorization = `Bearer ${token}`
+            api.defaults.headers.common['Authorization'] = `Bearer ${token}`
 
             setData({
                 token,
@@ -48,10 +49,29 @@ function AuthProvider({ children }){
             })
         }
     }, [])
+
+
+    async function updateProfile({ user }){
+        try {
+            await api.put('/users', user)
+            localStorage.setItem('@rocketnotes:user', JSON.stringify(user))
+
+            setData({ user, token: data.token})
+            alert('Perfil atualizado!')
+        } catch (error) {
+            if(error.response){
+                alert(error.response.data.message)
+            } else {
+                alert("Nao foi possivel atualizar o perfil")
+            }
+        }
+    }
+
     return (
         <AuthContext.Provider value={{
             signIn, 
             signOut,
+            updateProfile,
             user: data.user,
             }}>
             {children}
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -6,8 +6,27 @@ import { Button } from '../../components/Button'
 import { Container, Form, Avatar  } from "./styles";
 
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import { useAuth } from '../../hooks/auth';
 
 export function Profile() {
+  const { user, updateProfile } = useAuth()
+
+  const [name, setName] = useState(user.name)
+  const [email, setEmail] = useState(user.email)
+  const [passwordOld, setPasswordOld] = useState()
+  const [passwordNew, setPasswordNew] = useState()
+
+  async function handleUpdate(){
+    const user = {
+      name, 
+      email, 
+      password: passwordNew, 
+      old_password: passwordOld,
+    }
+    await updateProfile({ user })
+  }
+
   return (
     <Container>
       <header>
@@ -35,27 +54,33 @@ export function Profile() {
           placeholder="Nome"
           type="text"
           icon={FiUser}
+          value={name}
+          onChange={e => setName(e.target.value)}
         />
 
         <Input
           placeholder="E-mail"
           type="text"
           icon={FiMail}
+          value={email}
+          onChange={e => setEmail(e.target.value)}
         />
 
         <Input
           placeholder="Senha atual"
           type="password"
           icon={FiLock}
+          onChange={e => setPasswordOld(e.target.value)}
         />
 
         <Input
           placeholder="Nova atual"
           type="password"
           icon={FiLock}
+          onChange={e => setPasswordNew(e.target.value)}
         />
 
-        <Button title="Salvar" />
+        <Button title="Salvar" onClick={handleUpdate}/>
       </Form>
     </Container>
   )
