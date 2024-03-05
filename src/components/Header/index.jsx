ccded610a4d279defafe3288d7159e5708096d25
rodrigoaugusto99 <rodrigoaugusto51@outlook.com>
2024-03-04 21:34:25 -0300

    carregando avatar no header tbm

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,13 +2,20 @@ import { RiShutDownLine } from 'react-icons/ri'
 import { Container, Profile, Logout } from './styles'
 import { useAuth } from '../../hooks/auth'
 
+import { api } from '../../services/api';
+import avatarPlaceholder from '../../assets/avatar_placeholder.svg'
+
 export function Header() {
-  const { signOut } = useAuth()
+
+  const { signOut, user } = useAuth()
+  const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder
+
   return (
     <Container>
       <Profile to='/profile'>
         <img
-          src="https://github.com/rodrigoaugusto99.png"
+          //src="https://github.com/rodrigoaugusto99.png"
+          src={avatarUrl}
           alt="Foto do usuário"
         />
 
