4a02b367a56c2a908b6483241d1bc96066fd89c1
rodrigoaugusto99 <rodrigoaugusto51@outlook.com>
2024-03-06 21:52:12 -0300

    carregando tags na home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,8 +6,34 @@ import { Section } from '../../components/Section'
 import { Note } from '../../components/Note'
 import { ButtonText } from '../../components/ButtonText'
 import { Input } from '../../components/Input'
+import { useState, useEffect } from 'react'
+import { api } from '../../services/api'
+
+//pras tags nao ficarem duplicadas, iremos fazer uma logica la no backend no banco
 
 export function Home() {
+  const [tags, setTags] = useState([])
+
+  //arrow func tion que recebe um vetor pra colocar os estados dependentes
+  //vazio pq nao queremos buscar o tempo todo pelas tags, vamos buscar apena
+  //na primeira vez que carregar a pagina
+
+  /*quando temos uma funcao que queremos reaprovietar ela, colcoamos
+  ela fora, mas como nesse caso vamos usar so aqui, podemos criar dentro
+  do useEffect. e chamamos a fuuncao la dentro mesmo. Pois nao podemos 
+  fazer async useEffect.*/
+  useEffect(() => {
+    //funcao que pega as apis
+    //e mete no setTags
+    async function fetchTags(){
+      const response = await api.get('/tags')
+      setTags(response.data)
+    }
+    fetchTags()
+  }, [])
+
+  /*tags && pra certificar-se que tem conteudo ali dentro 
+  faz o mapeamento com o map iterando e criando cada tag(ButtonText)*/
   return (
     <Container>
       <Brand>
@@ -17,9 +43,30 @@ export function Home() {
       <Header></Header>
 
       <Menu>
-        <li><ButtonText title="Todos" $isactive /></li>
-        <li><ButtonText title="React" /></li>
-        <li><ButtonText title="Nodejs" /></li>
+        
+        {
+          tags && tags.map(tag =>(
+            //chave vms colocar o id pois eh unico la no sqlite
+            //String() pois eh o padrao p key
+            <li key={String(tag.id)}>  
+              <ButtonText 
+                title={tag.name} 
+              />
+            </li>
+          ))
+        }
+        <li>
+          <ButtonText 
+            title="Todos" 
+            $isactive 
+          />
+        </li>
+        
+        <li>
+          <ButtonText 
+            title="Nodejs"
+          />
+        </li>
       </Menu>
 
       <Search>
