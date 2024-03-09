a4e488a5ef3fc21d91585be8d5f67203efe56736
rodrigoaugusto99 <rodrigoaugusto51@outlook.com>
2024-03-09 16:29:34 -0300

    ajuste para voltar p/ tela inicial ao deslogar

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,21 @@
 import { RiShutDownLine } from 'react-icons/ri'
 import { Container, Profile, Logout } from './styles'
 import { useAuth } from '../../hooks/auth'
-
 import { api } from '../../services/api';
 import avatarPlaceholder from '../../assets/avatar_placeholder.svg'
+import { useNavigate } from 'react-router-dom';
 
 export function Header() {
 
   const { signOut, user } = useAuth()
   const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder
+
+  //quando deslogar, vamos primeiro chamar o navigate pra tela inciial antes de chamar o signOut do useAuth
+  const navigate = useNavigate()
+  function handleSignOut(){
+    navigate('/')
+    signOut()
+  }
 
   return (
     <Container>
@@ -25,7 +32,7 @@ export function Header() {
         </div>
       </Profile>
 
-      <Logout onClick={signOut}>
+      <Logout onClick={handleSignOut}>
         <RiShutDownLine />
       </Logout>
     </Container>
